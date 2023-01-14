02e2b0227c0c8d279e4bbb4ffe637d7f1e6fb4f8
Cameron <29291525+cameronnz@users.noreply.github.com>
2023-01-14 18:00:40 +1300

    Initial

---
+++ js/about.4725e299.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvehiclesite"]=self["webpackChunkvehiclesite"]||[]).push([[443],{7381:function(e,n,t){t.r(n),t.d(n,{default:function(){return o}});var s=t(3396);const u={class:"about"},i=(0,s._)("h1",null,"This is an about page",-1),a=[i];function c(e,n){return(0,s.wg)(),(0,s.iD)("div",u,a)}var r=t(89);const l={},h=(0,r.Z)(l,[["render",c]]);var o=h}}]);
+//# sourceMappingURL=about.4725e299.js.map
